b97dc78a34cfa353d9e0691f66fb230a0b7c18cc
Ashish-Thanki <ashishthanki95@gmail.com>
2020-07-19 13:51:18 +0100

    git commit -m 'Initial Commit'

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -1,12 +1,11 @@
 module.exports = {
-  siteTitle: 'Hi! I\'m Ashish!',
+  siteTitle: '',
   siteDescription: `About Ashish Thanki`,
   keyWords: ['ashish', 'thanki', 'curriculum'],
   authorName: 'Ashish Thanki',
   githubUsername: 'ashishthanki',
   authorAvatar: '/images/avatar.jpeg',
-  authorDescription: `A professionally registered mechanical engineering currently working as a 
-     <strong> Data Scientist</strong>.<br /><br />
+  authorDescription: `A <strong>Data Scientist</strong> and professionally registered mechanical engineer..<br /><br />
     
     Before becoming a Data Scientist, Ashish was a Mechancial Engineer for WSP in London,
     where he designed several large commercial office buildings. <br /><br />
@@ -16,7 +15,7 @@ module.exports = {
 
     Outside of work, he fell in love with Machine Learning and why he drove to transition his 
     career from Mechanical Engineering to Data Science.<br /><br />
-    This website showcases Ashish's career, GitHub repos and ... a Blog. <br /><br />`,
+    This website showcases Ashish's career and GitHub repository.<br /><br />`,
   skills: [
     {
       name: 'Python',
@@ -24,23 +23,19 @@ module.exports = {
     },
     {
       name: 'Git',
-      level: 70
-    },
-    {
-      name: 'Pandas',
-      level: 70
-    },
-    {
-      name: 'Numpy',
-      level: 70
+      level: 80
     },
     {
       name: 'Sci-kit Learn',
       level: 90
     },
     {
+      name: 'XGBoost',
+      level: 90
+    },
+    {
       name: 'TensorFlow2',
-      level: 80
+      level: 90
     },
     {
       name: 'HTML',
@@ -51,19 +46,16 @@ module.exports = {
       level: 50
     },
     {
-      name: 'Javascript',
-      level: 40
+      name: 'Further Mathematics',
+      level: 95
     },
     {
-      name: 'NodeJs',
-      level: 40
-    }
-    
-    
-    /* more skills here */
+      name: 'Statistics',
+      level: 95
+    },
   ],
   jobs: [
-    /* more jobs here */
+    
     {
       company: "WSP",
       begin: {
@@ -72,40 +64,141 @@ module.exports = {
       },
       duration: null,
       occupation: "Data Scientist",
-      description: "Responsible for the HCC Future Mobility Transportation Plan."
+      description: "Responsible for the Hertfordshire County Council's Future Mobility Transportation Plan."
   
     },  {
-      company: "WSP",
+      company: "EdX",
       begin: {
-        month: 'sep',
-        year: '2017'
+        month: 'May',
+        year: '2020'
       },
-      duration: null,
-      occupation: "Mechancial Engineer",
-      description: "Responsible for the Building Services for multiple commercial office buildings."
+      duration: 'Qualification',
+      occupation: "Microsoft Power BI Certificate ",
+      description: "Gained an intuition on business decisions by analyzing enterprise data for insights and data visualization." 
   
     },  {
       occupation: "Head of Young MEP Engineers",
       begin: {
         month: 'Jan',
         year: '2018'
-      }
+      },
+      duration: '1 year',
+      description: "Lead the restructuring of the organisation and professionally registered 95% of the forum with many Institutions including IMechE, IEEE, IET, CIBSE, SoPHE. The highest number of professionally registered engineers within the department."
     },  {
-      company: "Graduated with a Masters from a Globally Leading University ",
+      company: "WSP",
       begin: {
-        month: 'sep',
+        month: 'Sep',
+        year: '2017'
+      },
+      duration: '3 years',
+      occupation: "Mechanical Engineer",
+      description: "Working on the Goldman Sachs London Development project and the Iconic 22 Bishopsgate."
+  
+    },  {
+      company: "Southampton City Council",
+      begin: {
+        month: 'Sep',
+        year: '2016'
+      },
+      duration: '1 year',
+      occupation: "Student Researcher",
+      description: "Led the engineering development and data analysis to allow the electrication of the council's fleet and established suitable charging point locations."
+  
+    },  {
+      company: "IBM Consultant Programme",
+      begin: {
+        month: 'Sep',
+        year: '2016'
+      },
+      duration: '9 months',
+      occupation: "Project Manager",
+      description: "Supported the NHS Southern Health Service Procurement Team to improve public trust."
+  
+    },  {
+      company: "Univeristy Business Challenge",
+      begin: {
+        month: 'Sep',
+        year: '2014'
+      },
+      duration: '8 months',
+      occupation: "Group Leader",
+      description: "The only team from Southampton to have won the group stages of the internationally recognised Business simulation."
+  
+    },    {
+      company: "Alexander Rosse",
+      begin: {
+        month: 'Jun',
+        year: '2014'
+      },
+      duration: '4 Months',
+      occupation: "Analyst",
+      description: "Created a Customer Relations system and database to establish connections with clients."
+  
+    },  {
+      company: "Institution of Mechanical Engineers",
+      begin: {
+        month: 'Mar',
+        year: '2014'
+      },
+      duration: '1 year',
+      occupation: "University Liaison Officier",
+      description: "Elected memeber and engaged with UCL, Imperial, Brunel and King's College Universities to drive student engagement, assess new technologies and advance engineering research to empower students to better understand what Mechancial Engineers do in industry."
+  
+    },    {
+      company: "Univeristy",
+      begin: {
+        month: 'Sep',
+        year: '2014'
+      },
+      duration: '2 years',
+      occupation: "Course Representative",
+      description: "Led the development of the course to maintain #1 Mechanical Engineering U.K. ranking."
+  
+    },  {
+      company: "a Globally Leading University ",
+      begin: {
+        month: 'Sep',
         year: '2013'
       },
-      duration: '4 yrs e 3 mos',
+      duration: '4 years',
       occupation: "Mechanical Engineering Masters Student",
-      description: "Developed a solid foundation on problem solving."
+      description: "Developed a solid foundation on Problem Solving, Python and Engineering from the University of Southampton."
   
+    },  {
+      company: "A-Levels",
+      begin: {
+        month: 'Aug',
+        year: '2013'
+      },
+      duration: 'Qualification',
+      occupation: "Achieved A*A*A",
+      description: "Highest achieving student at Further Mathematics with 100% in 3 exams and 90%+ in 5 exams, with 91% average."
+
+    },  {
+    company: "Mathematics A-Levels",
+    begin: {
+      month: 'Aug',
+      year: '2012'
     },
+    duration: 'Qualification',
+    occupation: "Achieved A*",
+    description: "Selected to take Mathematics A-Level a year early."
+  },  {
+    company: "Kumon",
+    begin: {
+      month: 'Jun',
+      year: '2010'
+    },
+    duration: '3 years and 4 Months',
+    occupation: "Further Mathematics and Statistics Teacher",
+    description: "Teaching students of all ages from 5 to 19 years old."
+
+  },
   ],
   portifolio: [
     {
       image: "/images/awesome-grid.png",
-      description: "End to End Data Science Project",
+      description: "Transportation Project",
       url: "https://github.com/ashishthanki/MLProjects/blob/master/SupervisedLearning/Transport/_InterstateTraffic.ipynb"
     },
     /* more portifolio items here */
@@ -117,14 +210,14 @@ module.exports = {
   },
   siteUrl: 'https://github.com/ashishthanki/ashishthanki.github.io',
   pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
-  siteCover: '/images/cover.jpeg',
+  siteCover: '/images/cover.JPG',
   googleAnalyticsId: 'UA-000000000-1',
   background_color: '#ffffff',
   theme_color: '#25303B',
   fontColor: "#000000cc",
   enableDarkmode: true, // If true, enables dark mode switch
   display: 'minimal-ui',
-  icon: 'src/assets/gatsby-icon.png',
+  icon: 'src/assets/icon.png',
   headerLinks: [
     {
       label: 'Home',
