d0c359952bb8eab0414724816e871f81f2d72f7a
Ashish-Thanki <ashishthanki95@gmail.com>
2020-07-19 02:12:41 +0100

    Initial Commit

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -5,30 +5,19 @@ module.exports = {
   authorName: 'Ashish Thanki',
   githubUsername: 'ashishthanki',
   authorAvatar: '/images/avatar.jpeg',
-  authorDescription: `A professionally registered mechanical engineering currently working as a <strong> Data Scientist</strong>. 
-    Before becoming a Data Scientist, Ashish was a Mechancial Engineer for WSP in London. He was involved in numerous iconic engineering 
-    designs across London and the UK. Ashish designed large commercial office buildings and was named as the 
-    Head of Early Profressionals, where he served as the leader and played a key role in representing young engineers across the business.
-    Outside of work, he fell in love with Machine Learning and why he drove to transition his career from Mechanical Engineering to 
-    Data Science.<br />
-    This website showcases Ashish's career, GitHub repos and ... a Blog. <br />`,
+  authorDescription: `A professionally registered mechanical engineering currently working as a 
+     <strong> Data Scientist</strong>.<br /><br />
+    
+    Before becoming a Data Scientist, Ashish was a Mechancial Engineer for WSP in London,
+    where he designed several large commercial office buildings. <br /><br />
+    During his time as a Mechanical Engineer, he was named as the Head of Early Profressionals,
+    where he served as the leader and played a key role in representing young engineers across 
+    the business.<br /><br />
+
+    Outside of work, he fell in love with Machine Learning and why he drove to transition his 
+    career from Mechanical Engineering to Data Science.<br /><br />
+    This website showcases Ashish's career, GitHub repos and ... a Blog. <br /><br />`,
   skills: [
-    {
-      name: 'HTML',
-      level: 70
-    },
-    {
-      name: 'CSS',
-      level: 50
-    },
-    {
-      name: 'Javascript',
-      level: 40
-    },
-    {
-      name: 'NodeJs',
-      level: 40
-    },
     {
       name: 'Python',
       level: 90
@@ -52,12 +41,40 @@ module.exports = {
     {
       name: 'TensorFlow2',
       level: 80
+    },
+    {
+      name: 'HTML',
+      level: 70
+    },
+    {
+      name: 'CSS',
+      level: 50
+    },
+    {
+      name: 'Javascript',
+      level: 40
+    },
+    {
+      name: 'NodeJs',
+      level: 40
     }
+    
+    
     /* more skills here */
   ],
   jobs: [
     /* more jobs here */
     {
+      company: "WSP",
+      begin: {
+        month: 'Aug',
+        year: '2020'
+      },
+      duration: null,
+      occupation: "Data Scientist",
+      description: "Responsible for the HCC Future Mobility Transportation Plan."
+  
+    },  {
       company: "WSP",
       begin: {
         month: 'sep',
@@ -68,28 +85,15 @@ module.exports = {
       description: "Responsible for the Building Services for multiple commercial office buildings."
   
     },  {
-      company: "Head of Young MEP Engineers",
+      occupation: "Head of Young MEP Engineers",
       begin: {
         month: 'Jan',
         year: '2018'
-      },
-      duration: null,
-      occupation: "Mechancial Engineer",
-      description: "Deployed on site and became the residential engineer for Goldman Sachs London HQ."
-  
-    }, {
-      company: "WSP",
-      begin: {
-        month: 'Mar',
-        year: '2019'
-      },
-      duration: '1 yr e 5 mos',
-      occupation: "Mechanical Design Engineer",
-      description: "Lead the Mechanical design for Santander's Digital hub and started the 300 people Auditorium design."
-    }, {
+      }
+    },  {
       company: "Graduated with a Masters from a Globally Leading University ",
       begin: {
-        month: 'Sept',
+        month: 'sep',
         year: '2013'
       },
       duration: '4 yrs e 3 mos',
@@ -101,8 +105,8 @@ module.exports = {
   portifolio: [
     {
       image: "/images/awesome-grid.png",
-      description: "Responsive grid for ReactJS",
-      url: "https://github.com/santosfrancisco/react-awesome-styled-grid"
+      description: "End to End Data Science Project",
+      url: "https://github.com/ashishthanki/MLProjects/blob/master/SupervisedLearning/Transport/_InterstateTraffic.ipynb"
     },
     /* more portifolio items here */
   ],
