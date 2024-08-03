eaa86aadd916e710dc97ca723d89a25b4e5731f2
Ashish Thanki <ashishthanki95@gmail.com>
2024-08-03 01:56:28 -0700

    FIx footer and add avatar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
-import Link from './Link'
-import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
+import siteMetadata from '@/data/siteMetadata'
+import Link from './Link'
 
 export default function Footer() {
   return (
@@ -25,8 +25,8 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
+          <Link href="/">
+            Ashish Thanki Codes
           </Link>
         </div>
       </div>
