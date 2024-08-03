07a85d7ad66d4c4be866fc3be66e14c0a153c9fb
Ashish Thanki <ashishthanki95@gmail.com>
2024-08-03 02:22:33 -0700

    Fix footer bug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,9 +25,7 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="/">
-            Ashish Thanki Codes
-          </Link>
+          <Link href="/">Ashish Thanki Codes</Link>
         </div>
       </div>
     </footer>
